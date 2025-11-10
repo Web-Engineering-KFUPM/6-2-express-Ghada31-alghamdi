4d70f7bccfcab05fa6d775e49b7072448cc5d095
Ghada31-alghamdi <s202269300@kfupm.edu.sa>
2025-11-10 11:53:10 +0300

    TODO5_all_Done

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -3,14 +3,17 @@ import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
 const PORT=3000;
 const app = express();
-
+app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
 
 app.use(cors());
 
 
-{/*write code to define routes*/}
-
-
-app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
+app.get("/", (req, res) => {
+      res.send("Welcome to the Quote Generator API");
+    });
+app.get("/api/quote", (req, res) => {
+      const quote = getRandomQuote();
+      res.json({ quote });
     });
