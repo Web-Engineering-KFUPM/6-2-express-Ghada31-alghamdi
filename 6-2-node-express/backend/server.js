481e9badb1a98739fb6fead194ee1d9467cbe1b0
Ghada31-alghamdi <s202269300@kfupm.edu.sa>
2025-11-10 11:38:08 +0300

    TODO4_done

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -3,14 +3,14 @@ import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
 const PORT=3000;
 const app = express();
-app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
 
-{/*write code for cors*/}
+
+app.use(cors());
 
 
 {/*write code to define routes*/}
 
 
-{/*write code to create server*/}
+app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
